1596d32dc21ee96755f8a6434750afb9cad1acc7
MUUK <90867079+MUUKGIZMO@users.noreply.github.com>
2025-03-06 17:31:58 -0500

    Calculator Python File

---
+++ calculator.py
@@ -0,0 +1,9 @@
+#Hi. This is Mynia's first python project on windows.
+# The dates is 03/06/2025.
+# I will be designing a calculator app...>.>
+import random
+import sys
+import math
+
+print("CALCULATOR")
+
