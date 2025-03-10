3015e63a0688e1bc4e5bb7b7bc9b9272782564d8
MUUK <90867079+MUUKGIZMO@users.noreply.github.com>
2025-03-09 23:12:05 -0400

    Delete calculator.py
    New file edited

--- calculator.py
+++
@@ -1,9 +0,0 @@
-#Hi. This is Mynia's first python project on windows.
-# The dates is 03/06/2025.
-# I will be designing a calculator app...>.>
-import random
-import sys
-import math
-
-print("CALCULATOR")
-
